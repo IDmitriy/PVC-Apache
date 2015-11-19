fda66aa2c15090e821a52920ac2777fafe8cb3bd
dmitriy.ivan <dmitriy.ivan@live.ru>
2015-11-19 14:51:22 +0300

    relocation

--- install.bat
+++ install.bat
@@ -4,5 +4,5 @@ set servicename=%~p0
 set servicename=%servicename:\=_%
 set servicename=%servicename:~1,-1%
 @echo on
-%~dp0/../bin/httpd.exe -k install -n %servicename%
+%~dp0/bin/httpd.exe -k install -n %servicename%
 pause
